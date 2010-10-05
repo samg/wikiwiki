5746885bd5da06bea70b8648d68b681281659a42
Sam Goldstein <sam@aboutus.org>
2010-10-05 16:55:38 -0700

    Don't show empty paths

--- lib/w2/controller.rb
+++ lib/w2/controller.rb
@@ -21,6 +21,7 @@ module W2
     end
 
     get %r'(.*)' do |path|
+      path = '/' if path == ''
       if File.exist? file_path(path)
         haml :show, :locals => {:path => path, :text => parsed_wiki_text_for(path)}
       else 
