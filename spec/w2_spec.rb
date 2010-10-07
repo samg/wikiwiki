b81ae7f455bede40b77f44cbfde0b13320173193
Sam Goldstein <sam@aboutus.org>
2010-10-06 21:47:13 -0700

    save_file tests

--- spec/w2_spec.rb
+++ spec/w2_spec.rb
@@ -3,129 +3,147 @@ require 'spec'
 require 'rack/test'
 
 set :environment, :test
-
-describe 'The WikiWiki App' do
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
-  end
-
-  def redirect_to(expected)
-    simple_matcher "to redirect to #{expected}" do |given|
-      (300..399) === given.status and
-      given.headers['Location'].should == expected
-    end
-  end
-  it "should redirect edit home page to '/'" do
-    post '/edit'
-    last_response.should redirect_to('/')
-  end
-
-end
-
-describe W2::Helpers do
+describe "WikiWiki" do
   before do
-    @helper = Object.new
-    @helper.extend W2::Helpers
-  end
-  def helper
-    @helper
+    FileUtils.rm_rf(File.dirname(__FILE__) + '/../wiki-test')
   end
 
-  describe "path_to_safe_filename" do
-    it "should convert /'s to ?'s" do
-      helper.path_to_safe_filename('foo/bar/baz').
-        should == 'foo?bar?baz'
+  describe 'The WikiWiki App' do
+    include Rack::Test::Methods
+
+    def app
+      Sinatra::Application
     end
 
-    it "should strip and squeeze slashes/question marks" do
-      helper.path_to_safe_filename('///foo//bar////baz//').
-        should == '?foo?bar?baz'
+    def redirect_to(expected)
+      simple_matcher "to redirect to #{expected}" do |given|
+        (300..399) === given.status and
+        given.headers['Location'].should == expected
+      end
+    end
+    it "should redirect edit home page to '/'" do
+      post '/edit'
+      last_response.should redirect_to('/')
     end
 
-    it "should conver spaces to _'s" do
-      helper.path_to_safe_filename('foo bar_baz').
-        should == 'foo_bar_baz'
-    end
   end
 
-  describe "revision_file_path" do
-    it %q"should convert /foo/bar to './lib/w2/../../wiki-test/.?foo?bar/#{now.to_i}.?foo?bar'" do
-      now = Time.now
-      Time.stub!(:now).and_return now
-      helper.revision_file_path('/foo/bar').
-        should == "./lib/w2/../../wiki-test/.?foo?bar/#{now.to_i}.?foo?bar"
+  describe W2::Helpers do
+    before do
+      @helper = Object.new
+      @helper.extend W2::Helpers
     end
-  end
-
-  describe 'changes_glob' do
-    it "should escape /'s" do
-      now = Time.now
-      Time.stub!(:now).and_return now
-      helper.changes_glob('/foo/bar').
-        should == './lib/w2/../../wiki-test/.\?foo\?bar/*'
+    def helper
+      @helper
     end
-  end
+    describe "save_file" do
+      it "should save a file" do
+        lambda do
+          helper.save_file('/save/a/file', 'some text')
+        end.should change{File.exist? helper.file_path('/save/a/file') }.
+          from(false).to true
+      end
 
-  describe "edit_path" do
-    it "should turn '/' => '/edit'" do
-      helper.edit_path('/').should == '/edit'
-    end
-
-    it "should turn '/edit' => '/edit'" do
-      helper.edit_path('/edit').should == '/edit'
+      it "should save the text" do
+        text = "Some\n==Awesome==\n [[WikiText]]"
+        helper.save_file('/save/a/file', text)
+        File.read(helper.file_path('/save/a/file')).should == text
+      end
     end
 
-    it "should turn 'foo' into 'foo/edit'" do
-      helper.edit_path('foo').should == 'foo/edit'
+    describe "path_to_safe_filename" do
+      it "should convert /'s to ?'s" do
+        helper.path_to_safe_filename('foo/bar/baz').
+          should == 'foo?bar?baz'
+      end
+
+      it "should strip and squeeze slashes/question marks" do
+        helper.path_to_safe_filename('///foo//bar////baz//').
+          should == '?foo?bar?baz'
+      end
+
+      it "should conver spaces to _'s" do
+        helper.path_to_safe_filename('foo bar_baz').
+          should == 'foo_bar_baz'
+      end
     end
 
-    it "should turn '/foo/bar' into 'foo/bar/edit'" do
-      helper.edit_path('/foo/bar').should == '/foo/bar/edit'
+    describe "revision_file_path" do
+      it %q"should convert /foo/bar to './lib/w2/../../wiki-test/.?foo?bar/#{now.to_i}.?foo?bar'" do
+        now = Time.now
+        Time.stub!(:now).and_return now
+        helper.revision_file_path('/foo/bar').
+          should == "./lib/w2/../../wiki-test/.?foo?bar/#{now.to_i}.?foo?bar"
+      end
     end
 
-    it "should turn '/foo/bar/edit' into 'foo/bar/edit'" do
-      helper.edit_path('/foo/bar/edit').should == '/foo/bar/edit'
+    describe 'changes_glob' do
+      it "should escape /'s" do
+        now = Time.now
+        Time.stub!(:now).and_return now
+        helper.changes_glob('/foo/bar').
+          should == './lib/w2/../../wiki-test/.\?foo\?bar/*'
+      end
     end
 
-    it "should turn '' into 'edit'" do
-      helper.edit_path('').should == 'edit'
+    describe "edit_path" do
+      it "should turn '/' => '/edit'" do
+        helper.edit_path('/').should == '/edit'
+      end
+
+      it "should turn '/edit' => '/edit'" do
+        helper.edit_path('/edit').should == '/edit'
+      end
+
+      it "should turn 'foo' into 'foo/edit'" do
+        helper.edit_path('foo').should == 'foo/edit'
+      end
+
+      it "should turn '/foo/bar' into 'foo/bar/edit'" do
+        helper.edit_path('/foo/bar').should == '/foo/bar/edit'
+      end
+
+      it "should turn '/foo/bar/edit' into 'foo/bar/edit'" do
+        helper.edit_path('/foo/bar/edit').should == '/foo/bar/edit'
+      end
+
+      it "should turn '' into 'edit'" do
+        helper.edit_path('').should == 'edit'
+      end
+
+      it "should turn 'edit' into 'edit'" do
+        helper.edit_path('edit').should == 'edit'
+      end
     end
 
-    it "should turn 'edit' into 'edit'" do
-      helper.edit_path('edit').should == 'edit'
-    end
-  end
+    describe "show_path" do
+      it "should turn '/edit' => '/'" do
+        helper.show_path('/edit').should == '/'
+      end
 
-  describe "show_path" do
-    it "should turn '/edit' => '/'" do
-      helper.show_path('/edit').should == '/'
-    end
+      it "should turn '/' => '/'" do
+        helper.show_path('/').should == '/'
+      end
 
-    it "should turn '/' => '/'" do
-      helper.show_path('/').should == '/'
-    end
+      it "should turn 'foo/edit' into 'foo'" do
+        helper.show_path('foo/edit').should == 'foo'
+      end
 
-    it "should turn 'foo/edit' into 'foo'" do
-      helper.show_path('foo/edit').should == 'foo'
-    end
+      it "should turn '/foo/bar/edit' into 'foo/bar'" do
+        helper.show_path('/foo/bar/edit').should == '/foo/bar'
+      end
 
-    it "should turn '/foo/bar/edit' into 'foo/bar'" do
-      helper.show_path('/foo/bar/edit').should == '/foo/bar'
-    end
+      it "should turn '/foo/bar/' into 'foo/bar'" do
+        helper.show_path('/foo/bar/').should == '/foo/bar'
+      end
 
-    it "should turn '/foo/bar/' into 'foo/bar'" do
-      helper.show_path('/foo/bar/').should == '/foo/bar'
-    end
+      it "should turn 'edit' into ''" do
+        helper.show_path('edit').should == ''
+      end
 
-    it "should turn 'edit' into ''" do
-      helper.show_path('edit').should == ''
-    end
-
-    it "should turn '' into ''" do
-      helper.show_path('').should == ''
+      it "should turn '' into ''" do
+        helper.show_path('').should == ''
+      end
     end
   end
 end
-
