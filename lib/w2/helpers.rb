8000910458fe7b9ff767fff2d3484db06eae6495
Sam Goldstein <sam@aboutus.org>
2010-11-21 09:31:33 -0800

    Normalize file endings without converting line breaks

--- lib/w2/helpers.rb
+++ lib/w2/helpers.rb
@@ -161,7 +161,7 @@ module W2
       else
         previous = ''
       end
-      previous, current = previous.gsub(/\r?\n/, "\n").chomp + "\n", current.gsub(/\r?\n/, "\n").chomp + "\n"
+      previous, current = previous.chomp + "\r\n", current.chomp + "\r\n"
       Dirb::Diff.new(previous, current).to_s(:html)
     end
 
